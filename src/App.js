115ccbb80a30db38c9ef9d2eb75a9f387de1802b
Lajja123 <lajja.lamprostech@gmail.com>
2024-01-25 12:15:12 +0530

    add react-helmet-async

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DaoRoadmap from "./pages/dao-roadmap/DaoRoadmap";
 import Hackathon from "./pages/programs/Hackathon";
 import React, { useState, useEffect } from "react";
-import { HelmetProvider } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -17,33 +17,38 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {loading ? (
-        <div class="center">
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-          <div class="wave"></div>
-        </div>
-      ) : (
-        <div className="App">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about-us" element={<AboutUs />}></Route>
-              <Route path="/programs/hackathon" element={<Hackathon />}></Route>
-              <Route path="/dao-roadmap" element={<DaoRoadmap />}></Route>
-            </Routes>
-          </Router>
-        </div>
-      )}
-    </div>
+    <HelmetProvider>
+      <div>
+        {loading ? (
+          <div class="center">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+          </div>
+        ) : (
+          <div className="App">
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about-us" element={<AboutUs />}></Route>
+                <Route
+                  path="/programs/hackathon"
+                  element={<Hackathon />}
+                ></Route>
+                <Route path="/dao-roadmap" element={<DaoRoadmap />}></Route>
+              </Routes>
+            </Router>
+          </div>
+        )}
+      </div>
+    </HelmetProvider>
   );
 }
 
